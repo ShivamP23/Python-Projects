d2275bab150253e6e6e9c935250ef3dc3deb5caa
ShivamP23 <shivampatel45001@gmail.com>
2024-10-22 14:56:23 -0400

    part 1

--- projects/turtle-crossing-start/main.py
+++ projects/turtle-crossing-start/main.py
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+screen.listen()
+screen.onkeypress(player.go_up, "Up")
+
+cars = CarManager()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    cars.create_car()
+    cars.move_car()
